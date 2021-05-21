13b0a91b4344b426925c3c4e285f0c238bdb5d4d
Elliott J. Humphreys <elliotthumphreys821@gmail.com>
2021-05-21 18:41:18 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "JohnProctorStylist",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-HMWCQHNKFD",
+      },
+    },
+  ],
+};
